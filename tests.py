af046ac727beeeee6f676c7b81755736de523a98
JonnySieben <121091527+JonnySieben@users.noreply.github.com>
2024-09-26 14:24:53 +0200

    .
    added code as instructed

--- tests.py
+++ tests.py
@@ -99,3 +99,36 @@ test__notenausgabe_value_error(101%, "sehr gut")
 
 # Prozentwert = "string"
 test__notenausgabe_type_error("null", "ungenügent")
+
+
+#####################################################################
+
+testdaten = [
+    (100, 'sehr gut'),
+    (92, 'sehr gut'),
+    (91, 'gut'),
+    (81, 'gut'),
+    (80, 'befriedigend'),
+    (67, 'befriedigend'),
+    (66, 'ausreichend'),
+    (50, 'ausreichend'),
+    (49, 'mangelhaft'),
+    (30, 'mangelhaft'),
+    (29, 'ungenügend'),
+    (0, 'ungenügend'),
+]
+
+throwData = [
+    (-1, ValueError),
+    (101, ValueError),
+    ('text', TypeError)
+]
+
+@pytest.mark.parametrize("prozent, erwartet", testdaten)
+def test_bewerte(prozent, erwartet):
+    assert prozentwert(prozent) == erwartet
+
+@pytest.mark.parametrize("prozent, erwartet", throwData)
+def test_bewerte_Throw(prozent, erwartet):
+    with pytest.raises(erwartet):
+        notenstring(prozent)
